f3b684e64b974818e9b4d9d7262fc094643f4011
Hasban Fardani <RemajaNgodingChannel@gmail.com>
2022-08-06 22:20:03 +0700

    try to deploy on vercel

---
+++ api/index.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+//	func main() {
+//		e := echo.New()
+//		e.Static("/", "public")
+//		e.Static("/icons", "static/icons")
+//		e.Logger.Fatal(e.Start(":1323"))
+//	}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	e := echo.New()
+	e.Static("/", "public")
+	e.Static("/icons", "static/icons")
+
+	e.ServeHTTP(w, r)
+}
--- content/post/rich-content.md
+++ content/post/rich-content.md
@@ -23,7 +23,8 @@ Hugo ships with several [Built-in Shortcodes](https://gohugo.io/content-manageme
 
 ## YouTube Privacy Enhanced Shortcode
 
-{{< youtube izYiDDt6d8s >}}
+{{< youtube id="rfscVS0vtbw" autoplay=true >}}
+
 
 <br>
 
